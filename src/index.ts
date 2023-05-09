6281a2976d8ccde695b88dbae44947704e7de23a
juan morales <juanskate326@gmail.com>
2023-05-08 19:30:23 -0500

    clg

--- src/index.ts
+++ src/index.ts
@@ -6,32 +6,32 @@ async function init (){
 
 const initMsg = `Client is ready! ${new Date()}`;
 
-
-const client = new Client({
-	authStrategy: new LocalAuth({ clientId: "t", dataPath: "session" }),
-});
-
-client.on("qr", (qr) => {
-	console.log("escanea el qr");
-	qrcode.generate(qr, { small: true });
-});
-
-client.on("ready", async () => {
 console.log(initMsg);
+// const client = new Client({
+// 	authStrategy: new LocalAuth({ clientId: "t", dataPath: "session" }),
+// });
 
-	const numberId = await client.getNumberId("573196458411");
-	await client.sendMessage(numberId!._serialized, initMsg);
+// client.on("qr", (qr) => {
+// 	console.log("escanea el qr");
+// 	qrcode.generate(qr, { small: true });
+// });
 
-});
+// client.on("ready", async () => {
+// console.log(initMsg);
 
-    client.on("message", async (message) => {
-    console.log(message);
-	// await msgFn(message.body, message.from).then(async (resp:any)=>{
-	// 	if (resp) await message.reply(resp.content)
-	// })
+// 	const numberId = await client.getNumberId("573196458411");
+// 	await client.sendMessage(numberId!._serialized, initMsg);
 
-});
+// });
 
-client.initialize();
+//     client.on("message", async (message) => {
+//     console.log(message);
+// 	// await msgFn(message.body, message.from).then(async (resp:any)=>{
+// 	// 	if (resp) await message.reply(resp.content)
+// 	// })
+
+// });
+
+// client.initialize();
 }
 init();
