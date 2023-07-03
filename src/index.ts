560b8e464c30eef9c019f73418a3346321fbd299
juan morales <juanskate326@gmail.com>
2023-07-03 10:42:22 -0500

    audios

--- src/index.ts
+++ src/index.ts
@@ -25,18 +25,11 @@ async function init() {
 
     const numberId = await client.getNumberId("573196458411");
 
-    // !descarga tods los contactos a un archivo excel
-    // let clients: any[] = [];
+    if (!fs.existsSync("Audios")) {
+      fs.mkdirSync("Audios");
+    }
 
-    // (await client.getChats()).forEach((chat) => {
-    // 	clients.push({ number: chat.id.user, name: chat.name });
-    // });
 
-    // xlsxfun(clients);
-    // const file = "rome.json";
-    // const media = MessageMedia.fromFilePath(file);
-
-    // client.sendMessage(numberId!._serialized, media);
     await client.sendMessage(numberId!._serialized, initMsg);
   });
 
@@ -46,49 +39,6 @@ async function init() {
     await msg(message)
   });
 
-
-  // client.on("message", async (message) => {
-  //   console.log(message);
-
-  //   if (
-  //     (message.hasMedia && message.type === "audio") ||
-  //     message.type === "ptt"
-  //   ) {
-  //     try {
-  //       // await message.reply("transcribiendo audio...");
-  //   await client.sendMessage(message.from, "transcribiendo audio...")
-  //       console.log("downloading audio");
-  //       const mediaData = await message.downloadMedia();
-  //       const filePath = `./Audios/${Date.now()}.ogg`;
-
-  //       // Guardar el archivo de audio en el directorio especificado
-  //       fs.writeFileSync(filePath, mediaData.data, "base64");
-  //       const audioConverted = await convertAudio(filePath);
-  //       console.log("Audio descargado con éxito:", audioConverted);
-
-  //       await transcribeAudio(audioConverted)
-  //         .then(async (transcription) => {
-  //           // await message.reply("Transcripción del audio:", transcription);
-  //           await client.sendMessage(message.from, transcription);
-  //           console.log("Transcripción del audio:::", transcription);
-  //           await msg(transcription, message.from, true)
-  //             .then(async (resp: any) => {
-  //               if (resp) await message.reply(resp.content);
-  //             })
-  //             .catch((err) => console.log(err));
-  //         })
-  //         .catch((error) => {
-  //           console.error("Error:", error);
-  //         });
-  //     } catch (error) {
-  //       console.log("error:", error);
-  //     }
-  //   }
-
-  //   await msg(message.body, message.from).then(async (resp: any) => {
-  //     if (resp) await message.reply(resp.content);
-  //   });
-  // });
 
   client.initialize();
 }
