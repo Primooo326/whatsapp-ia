25db996a1f4c1b421bde1a4daf57d07f5e3dfed7
juan morales <juanskate326@gmail.com>
2023-07-12 11:42:02 -0500

    CONFIG

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ import { config } from "dotenv";
 config();
 
 const PORT = process.env.PORT || 3000;
-const APIKEY = process.env.APIKEY || "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";
+const APIKEY = process.env.APIKEY || "YOUR_API_KEY";
 export default {PORT, APIKEY}
