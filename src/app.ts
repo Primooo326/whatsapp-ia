77f7b2354110ba1cb711b8b2cbddd7d20b38d79c
juan morales <juanskate326@gmail.com>
2023-07-25 20:21:34 -0500

    show qr

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import { Client, LocalAuth } from "whatsapp-web.js";
 import qrcode from "qrcode-terminal";
 import msg from "./messages2";
 import fs from "fs";
-
+const fetch = require('node-fetch');
+const { createCanvas, loadImage } = require('canvas');
 console.log("initializing application");
 async function init() {
   const initMsg = `Client is ready! ${new Date()}`;
@@ -14,12 +15,12 @@ async function init() {
     }
   });
 
-  client.on("qr", (qr) => {
+  client.on("qr", async (qr) => {
     console.log("escanea el qr");
     const codeHttp = "https://chart.googleapis.com/chart?cht=qr&chs=177x177&choe=utf-8&chl="+qr
 
     console.log(codeHttp);
-
+    fetchQRCodeAndShow();
     qrcode.generate(qr, { small: true });
     return new Response(codeHttp)
   });
@@ -57,4 +58,41 @@ try {
 } catch (error) {
   console.log(error);
   init();
+}
+
+
+function fetchQRCodeAndShow() {
+  const url = 'https://chart.googleapis.com/chart?cht=qr&chs=177x177&choe=utf-8&chl=holamundo';
+
+  // Hacemos la petición utilizando node-fetch
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.buffer();
+    })
+    .then(imageBuffer => {
+      // Creamos el lienzo (canvas) y la imagen a partir del buffer recibido
+      const canvas = createCanvas(177, 177);
+      const context = canvas.getContext('2d');
+
+      loadImage(imageBuffer).then(image => {
+        // Dibujamos la imagen en el lienzo
+        context.drawImage(image, 0, 0, 177, 177);
+
+        // Creamos una ventana gráfica para mostrar el código QR
+        const { registerFont } = require('canvas');
+        registerFont('./path/to/your-font-file.ttf', { family: 'YourFontName' }); // Reemplaza con una fuente compatible
+
+        const window = require('window');
+        const win = new window.Window();
+
+        // Mostramos el lienzo en la ventana gráfica
+        win.document.body.appendChild(canvas);
+      });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 }
